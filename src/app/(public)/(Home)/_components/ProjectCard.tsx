e6ce4f3fef55d62a66f6957e96387666884cf950
MoizSheikh <moizshaikh83@gmail.com>
2025-01-21 23:54:25 +0500

    project page changes

--- src/app/(public)/(Home)/_components/ProjectCard.tsx
+++ src/app/(public)/(Home)/_components/ProjectCard.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { Project } from "@/app/_lib/_types/genericTypes";
 import { ExternalLinkSVG, Github } from "@/app/_lib/assets/HomepageSVG";
-import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import Link from "next/link";
-import { FiAlertCircle } from "react-icons/fi";
 
 const ProjectCard = ({
   setSelected,
@@ -17,59 +14,57 @@ const ProjectCard = ({
     project;
 
   return (
-    <>
-      <div className="relative flex-1 rounded-xl border border-card-border bg-card-bg flex flex-col justify-start items-end p-4 min-w-4/12">
-        <div
-          className="w-full hover:-rotate-6 hover:-translate-y-8 transition-transform duration-300 cursor-pointer"
-          onClick={() => {
-            setSelected(project);
-          }}
-        >
-          <Image
-            src={img}
-            alt="project"
-            width={300}
-            height={0}
-            className="object-contain"
-            style={{ width: "100%", height: "200px" }}
-          />
+    <div className="relative flex-1 rounded-xl border border-card-border bg-card-bg flex flex-col justify-start items-end p-4 min-w-4/12">
+      <div
+        className="w-full hover:-rotate-6 hover:-translate-y-8 transition-transform duration-300 cursor-pointer"
+        onClick={() => {
+          setSelected(project);
+        }}
+      >
+        <Image
+          src={img}
+          alt="project"
+          width={300}
+          height={0}
+          className="object-contain"
+          style={{ width: "100%", height: "200px" }}
+        />
+      </div>
+      <div className="w-full my-2">
+        <div className="flex items-center gap-2 w-full">
+          <h4 className="font-bold text-lg shrink-0">{name}</h4>
+          <div className="w-full h-[1px] bg-zinc-600"></div>
+          {githubLink && (
+            <Github
+              size="40px"
+              className="cursor-pointer text-xl"
+              onClick={() => window.open(githubLink, "_blank")}
+            />
+          )}
+          {url && (
+            <ExternalLinkSVG
+              size="40px"
+              className="cursor-pointer"
+              onClick={() => window.open(url, "_blank")}
+            />
+          )}
         </div>
-        <div className="w-full my-2">
-          <div className="flex items-center gap-2 w-full">
-            <h4 className="font-bold text-lg shrink-0">{name}</h4>
-            <div className="w-full h-[1px] bg-zinc-600"></div>
-            {githubLink && (
-              <Github
-                size="40px"
-                className="cursor-pointer text-xl"
-                onClick={() => window.open(githubLink, "_blank")}
-              />
-            )}
-            {url && (
-              <ExternalLinkSVG
-                size="40px"
-                className="cursor-pointer"
-                onClick={() => window.open(url, "_blank")}
-              />
-            )}
-          </div>
-          <p className="text-[#a3a3a3] overflow-hidden text-ellipsis whitespace-nowrap w-full">
-            {techStack.join(" - ")}
-          </p>
+        <p className="text-[#a3a3a3] overflow-hidden text-ellipsis whitespace-nowrap w-full">
+          {techStack.join(" - ")}
+        </p>
 
-          <div>
-            <p className="text-sm text-zinc-300 ">{smallDescription}</p>
-            <div
-              role="button"
-              onClick={() => setSelected(project)}
-              className="text-indigo-300"
-            >
-              Learn More.
-            </div>
+        <div>
+          <p className="text-sm text-zinc-300 ">{smallDescription}</p>
+          <div
+            role="button"
+            onClick={() => setSelected(project)}
+            className="text-indigo-300"
+          >
+            Learn More.
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
