f8869a110b8f8f3d4b5b62352a05f9281e023917
MoizSheikh <moizshaikh83@gmail.com>
2025-02-01 01:38:07 +0500

    move up button added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./../app/_lib/_styles/globals.css";
+import ScrollToTop from "./_components/ScrollToTop";
 
 export const metadata: Metadata = {
   title: "ABDUL MOIZ SHEIKH",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex flex-col min-h-screen bg-white antialiased">
         <main className="flex-grow bg-white">{children}</main>
+        <ScrollToTop />
       </body>
     </html>
   );
